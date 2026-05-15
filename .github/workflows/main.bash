dfb5142e0e0e638088282774a232874e83669cbd
Epic <tractionbruh@gmail.com>
2026-05-15 13:28:49 +0100

    Create main.bash

---
+++ .github/workflows/main.bash
@@ -0,0 +1,4 @@
+git add ZalithLauncher/src/main/java/com/movtery/zalithlauncher/setting/AllSettings.kt
+git add ZalithLauncher/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
+git commit -m "fix: add Vulkan warning and force OpenGL settings"
+git push
